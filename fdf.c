90865acfe7fdbe5987515d10b134a42da12efc58
keuhdall <lmarques@student.42.fr>
2016-11-28 14:36:27 +0100

    display starting to look good, still a lot of work to do tho

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/22 16:46:42 by lmarques          #+#    #+#             */
-/*   Updated: 2016/11/25 16:39:59 by                  ###   ########.fr       */
+/*   Updated: 2016/11/28 14:22:10 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,9 +55,7 @@ int		ft_check_file(char *name)
 	return (1);
 }
 
-
-
-void	print_map(t_list *map)
+void	print_map(t_list *map, int len)
 {
 	t_list	*tmp;
 	int		count;
@@ -69,8 +67,7 @@ void	print_map(t_list *map)
 	{
 		count = 0;
 		count2 = 0;
-		//while (((int *)(tmp->content))[count])
-		while (count2 <= 10)
+		while (count2 < len)
 		{
 			printf("%d ", ((int *)(tmp->content))[count]);
 			count++;
@@ -86,7 +83,6 @@ void	print_tab(t_point *tab, int len)
 	int	count;
 
 	count = 0;
-	printf("LEN = %d\n", len);
 	while (count < len)
 	{
 		printf("x = %d y = %d\n", tab[count].x, tab[count].y);
@@ -94,28 +90,95 @@ void	print_tab(t_point *tab, int len)
 	}
 }
 
+void	ft_trace(t_meta mlx, t_point src, t_point dst)
+{
+	int	dx;
+	int	dy;
+	int	x;
+	int	y;
+
+	dx = dst.x - src.x;
+	dy = dst.y - src.y;
+	x = src.x;
+	while (x < dst.x)
+	{
+		y = src.y + dy * (x - src.x) / dy;
+		mlx_pixel_put(mlx.ptr, mlx.win, x, y, 0x00FF00);
+		x++;
+	}
+}
+
+void	ft_display_tab(t_meta mlx, t_point *tab, int len, int offset)
+{
+	int		count;
+	int		count_index;
+	int		tmp;
+	int		*tab_tmp;
+
+	count = 1;
+	count_index = 0;
+	tmp = tab[count].y;
+	if (!(tab_tmp = (int *)malloc(sizeof(int) * len)))
+		tab_tmp = NULL;
+	while (count_index < len)
+	{
+		tab_tmp[count_index] = tab[count_index].x;
+		count_index++;
+	}
+	count_index = 0;
+	while (count_index < len)
+	{
+		if (tab[count_index].y != tmp)
+		{
+			count = 1;
+			tmp = tab[count_index].y;
+		}
+		/*
+		mlx_pixel_put(mlx.ptr, mlx.win,
+		(count * offset / 2 - tab[count_index].y * offset / 2) + 150,
+		(count * offset / 2 + tab[count_index].y * offset / 2) + 150,
+		//(0x00FF00 + tab[count_index].x * 100));
+		0x00FF00);
+		*/
+		tab[count_index].x = (count * offset / 2 - tab[count_index].y *
+			offset / 2) + 150;
+		tab[count_index].y = (count * offset / 2 + tab[count_index].y *
+			offset / 2) + 150 - tab_tmp[count_index];
+		count++;
+		count_index++;
+	}
+	print_tab(tab, len);
+	count = 0;
+	while (count + 1 < len)
+	{
+		ft_trace(mlx, tab[count], tab[count + 1]);
+		count++;
+	}
+	/*
+	count = 0;
+	while (count + 11 < len)
+	{
+		ft_trace(mlx, tab[count], tab[count + 11]);
+		count += 11;
+	}
+	*/
+}
+
 int		main(int argc, char *argv[])
 {
 	int		err;
 	int		len;
+	t_meta	mlx;
 	t_point	*tab;
-	void	*mlx;
-	void	*window;
 
 	err = 0;
 	len = 0;
 	tab = ft_init_tab(argv[1], &err, &len);
-	print_tab(tab, len);
-	/*
-	if (argc != 2 || !ft_check_file(argv[1]))
-	{
-		ft_putendl("error");
-		return (0);
-	}
-	*/
-	mlx = mlx_init();
-	window = mlx_new_window(mlx, 800, 600, "test");
-	//mlx_loop(mlx);
+	//print_tab(tab, len);
+	mlx.ptr = mlx_init();
+	mlx.win = mlx_new_window(mlx.ptr, 800, 600, "FdF");
+	ft_display_tab(mlx, tab, len, 30);
+	mlx_loop(mlx.ptr);
 	argc++;
 	return (0);
 }
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/25 01:19:41 by lmarques          #+#    #+#             */
-/*   Updated: 2016/11/25 16:31:49 by                  ###   ########.fr       */
+/*   Updated: 2016/11/27 18:29:33 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@
 # include "./minilibx_macos/mlx.h"
 # include "./libft/libft.h"
 # include "./get_next_line/get_next_line.h"
+
+typedef struct	s_meta
+{
+	void		*ptr;
+	void		*win;
+}				t_meta;
 
 typedef struct	s_point
 {
